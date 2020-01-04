a113f8fca87176a91b6a14094be5b687c96d5e2a
Mohak Chugh <me.mohakchugh@gmail.com>
2020-01-04 11:53:03 +0530

    routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { CropfeedComponent } from './cropfeed/cropfeed.component';
+import { CropProductComponent } from './crop-product/crop-product.component';
+import { PaymentComponent } from './payment/payment.component';
+import { ShoppingcartComponent } from './shoppingcart/shoppingcart.component';
 
 const routes: Routes = [
   { path: 'login', component : LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'feed', component: CropfeedComponent}
+  { path: 'feed', component: CropfeedComponent },
+  { path: 'cropproduct', component: CropProductComponent },
+  { path: 'payment', component: PaymentComponent },
+  { path: 'cart', component: ShoppingcartComponent }
 
 ];
 
